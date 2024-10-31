df1bbb0c5c7aa16544057fd3ba1fe874598a15b9
pieceowater <pieceowater@gmail.com>
2024-10-31 11:56:08 +0500

    front shi

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="font-mono m-5">
+    <NuxtLayout>
+      <Header/>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
+
+<style>
+@import "tailwindcss";
+@import "@nuxt/ui";
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UButton block>Button</UButton>
+</template>
